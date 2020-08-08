0bad1dde74b808f341d64d2a94849a290484e269
mustadev <58852293+mustadev@users.noreply.github.com>
2020-08-08 22:28:04 +0100

    test modification layout using functions

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * Site branding wrapper and display
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	function storefront_site_branding() {
+	?>	
+		<div class="site-branding">
+	<?php	echo "Hello World" ?>
+		</div>
+		<?php
+	}
