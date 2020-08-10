5667ef37c45b8c4c9ed2c4cb3809328329d3e083
mustadev <58852293+mustadev@users.noreply.github.com>
2020-08-10 21:01:36 +0100

    dynamic topbar menu

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ function storefront_add_topbar() {
                 </form> -->
                 <?php storefront_product_search(); ?>
             </div>
-            <div class="topbar__nav">
+            <!-- <div class="topbar__nav">
                 <div class="topbar__nav__item">
                     <span class="topbar__nav__item__text" >Upload</span>
                     <i class="fas fa-upload"></i>
@@ -29,9 +29,28 @@ function storefront_add_topbar() {
                     <span class="topbar__nav__item__text">Account</span>
                     <i class="fas fa-user"></i>
                 </div>
-            </div>
+            </div> -->
+            <?php
+            wp_nav_menu(
+     
+                array( 'theme_location'=> 'topbar', 
+                'depth' => 0,
+                'container' => 'div',
+                'container_class' => 'topbar__nav__container', 
+                'menu_class' => 'topbar__nav__container__menu'
+                )
+            ); ?>
         </div>
         <?php 
 }
 
 add_action( 'storefront_before_header', 'storefront_add_topbar' );
+
+function register_topbar_menu() {
+    register_nav_menus( 
+        array(
+        'topbar' => __( 'TopBar Menu', 'omlil' )
+        ) 
+    );
+}
+add_action( 'after_setup_theme', 'register_topbar_menu' );
