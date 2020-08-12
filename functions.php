17261970e818896af6dd763fa86ecc41add5c078
mustadev <58852293+mustadev@users.noreply.github.com>
2020-08-12 15:28:06 +0100

    customization topbar background color

--- functions.php
+++ functions.php
@@ -90,3 +90,38 @@ function storefront_credit() {
 	</div><!-- .site-info -->
 	<?php
 }
+
+
+// Theme Customization
+
+function omlil_customize_register( $wp_customize ) {
+    //All our sections, settings, and controls will be added here
+    $wp_customize->add_setting( 'topbar_backgroundcolor' , array(
+        'default'   => '#000000',
+        'transport' => 'refresh',
+    ) );
+
+    $wp_customize->add_section( 'omlil_topbar' , array(
+        'title'      => __( 'Top Bar', 'omlil' ),
+        'priority'   => 30,
+    ) );
+    
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'link_color', array(
+        'label'      => __( 'Top Bar Color', 'omlil' ),
+        'section'    => 'omlil_topbar',
+        'settings'   => 'topbar_backgroundcolor',
+    ) ) );
+    
+ }
+ add_action( 'customize_register', 'omlil_customize_register' );
+
+
+ function omlil_customize_css()
+{
+    ?>
+         <style type="text/css">
+             .topbar { background-color: <?php echo get_theme_mod('topbar_backgroundcolor', '#000000'); ?>; }
+         </style>
+    <?php
+}
+add_action( 'wp_head', 'omlil_customize_css');
